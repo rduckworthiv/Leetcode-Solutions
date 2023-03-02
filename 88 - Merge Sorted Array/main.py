78ea8e2a97ba92a798c042f6419c7a265927ee29
Randall Duckworth IV <randall.duckworthiv@gmail.com>
2023-03-02 13:23:18 -0600

    forgot to hit save before committing lol

--- 88 - Merge Sorted Array/main.py
+++ 88 - Merge Sorted Array/main.py
@@ -4,5 +4,20 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-        for i in range(n):
-            
+        # merge from end, starting from m+n-1
+        last = m+n-1
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m -= 1
+            else:   # if nums1[m] <= nums2[n]
+                nums1[last] = nums2[n-1]
+                n -= 1
+            last -= 1
+
+        # if elements left in nums2, backfill nums1
+        while n > 0:
+            nums1[last] = nums2[n-1]
+            n -= 1
+            last -= 1
+        
